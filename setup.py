fe653a591301288c393f877cf387247534586947
AndreyChvanov <62611444+AndreyChvanov@users.noreply.github.com>
2024-04-15 15:05:36 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='tabular_dae',
+    name='my_tabular_dae',
     version='0.2',
     description='Denoise AutoEncoders for Tabular Data',
     url='https://github.com/ryancheunggit/tabular_dae',
